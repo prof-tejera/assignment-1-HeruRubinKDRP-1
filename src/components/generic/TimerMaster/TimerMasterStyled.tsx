ac5c1e3bb2977f2f777da95a9db14083b8b373d6
HeruRubinKDRP <herurubin@gmail.com>
2023-11-02 17:36:52 -0400

    Updates

--- src/components/generic/TimerMaster/TimerMasterStyled.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const TimerMasterStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  
-`
--- src/components/generic/common-styles/CommonStyles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
- import {css} from "styled-components";
-
- export const softShadowCSS = css`
-  border-radius: 5vw;
-  background: #e0e0e0;
-  box-shadow: 2vw 2vw 6vw #bebebe, -2vw -2vw 6vw #ffffff;
-  padding: 3vw;
- `
